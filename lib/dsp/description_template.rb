3341bd0629b923149a6de1f19431e3b4a10a99ee
hsioji <haisaioji@gmail.com>
2013-10-21 18:33:19 +0900

    statement_templatesメソッドの修正

--- lib/dsp/description_template.rb
+++ lib/dsp/description_template.rb
@@ -2,12 +2,24 @@ module DSP
   class DescriptionTemplate < Spira::Base
 
     include Spira::Resource
-    has_many :statement_templates, :predicate => RDF::RDFS.subClassOf, :type => "DSP::StatementTemplate"
+    #has_many :statement_templates, :predicate => RDF::RDFS.subClassOf, :type => "DSP::StatementTemplate"
     property :label, :predicate => RDF::RDFS.label, :type => String
     property :statement_order, :predicate => RDF::REG.statementOrder, :type => String
     property :id_field, :predicate => RDF::REG.idField, :type => String
     property :resource_class, :predicate => RDF::DSP.resourceClass, :type => RDF::URI
     #define_type RDF::DSP.DescriptionTemplate
 
+    def statement_templates
+      sparql =<<-EOF
+SELECT distinct ?statement_template_uri
+WHERE {
+  ?statement_template_uri a <http://purl.org/metainfo/terms/dsp#StatementTemplate> .
+}
+EOF
+      statement_templates = SPARQL.execute(sparql, Spira.repository(:default)).map do |solution|
+        ::DSP::DescriptionTemplate.for(RDF::URI.new(solution.statement_template_uri))
+      end
+      return statement_templates
+    end
   end
 end
